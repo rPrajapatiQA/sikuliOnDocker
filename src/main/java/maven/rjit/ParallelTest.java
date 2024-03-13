1f9c869fe07bc3228f5d467b9d70a846f664048f
ranjeet prajapati <100828972+rPrajapatiQA@users.noreply.github.com>
2024-03-13 15:19:35 +0530

    Update ParallelTest.java

--- src/main/java/maven/rjit/ParallelTest.java
+++ src/main/java/maven/rjit/ParallelTest.java
@@ -30,6 +30,8 @@ public class ParallelTest {
 			if (browser.equalsIgnoreCase("chrome")) {
 				WebDriverManager.chromedriver().setup();
 				ChromeOptions options = new ChromeOptions();
+				options.add_argument('--remote-debugging-pipe');
+				options.addArguments("--headless=new");
 				options.addArguments("--no-sandbox");
 				options.addArguments("--disable-dev-shm-usage");
 				//options.addArguments("--headless");
